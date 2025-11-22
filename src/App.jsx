d65f45ae219ea54cc4f536641a8e1fd4c72df487
Sivakumar <sivakumaar8158@gmail.com>
2025-11-22 09:12:50 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <>
+    <div className='bg-emerald-600 p-48 '>Welcome!!!</div>
+    </>
+  )
+}
+
+export default App;
